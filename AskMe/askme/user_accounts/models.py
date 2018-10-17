75636b251bd7a51f8c2c49d96c2f1fa3e931a8af
Keenan <KeenanKomoto@gmail.com>
2018-10-17 11:54:39 -0400

    Add the Testing Goat into environment (Selenium and geckodriver)

--- AskMe/askme/user_accounts/models.py
+++ AskMe/askme/user_accounts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class User(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
 
