74d71ead46f31d0a592ec29338ef077bb76ed82f
Keenan <KeenanKomoto@gmail.com>
2018-10-17 16:01:53 -0400

    FT (Functional Test) specs w/comments and includes python's unittest

--- AskMe/askme/goats/functional_tests.py
+++ AskMe/askme/goats/functional_tests.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_startretrieve(self):
+        # Person goes to online app.
+        browser.get('http://localhost:8000')
+        
+        self.asserIn('askme', self.browser.title)
+        self.fail('Finish the test!')
+        
+        # Person notices title and header mention something
+        
+        # Person tries to signup
+        
+        # Person successfully signs up and is brought to their homepage
+        
+        # Person browses the different surveys available
+        
+        # Person tries to take a new survey
+        
+        # Person successfully takes a new survey and gets to see their results compared to other peoples
+        
+        # Person logs out successfully 
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
